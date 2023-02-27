de68c87d65b0e8a2d5a315abd3f88fa964d49f46
viksinno <vikas.prasad@innoraft.com>
2023-02-27 19:07:18 +0530

    FT2023-35: ALL TASK DONE.

---
+++ php_oops/Task3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="./jquery.js"></script>
+</head> 
+<?php include 'output.php';?>
+
+<body>
+<form  method="post" enctype="multipart/form-data">
+FirstName: <input class ="fname" type="text" name="firstname" required><br>
+LastName: <input class="lname" type="text" name="lastname" required><br>
+FullName: <input class="fullname" type="text" readonly name="fullname"><br>
+Image: <input type="file" name="image"> <br><br>
+TextArea: <Textarea name="content"  cols="30" rows="10"></Textarea>
+</form>
+<script>
+    $(document).ready(function(){
+    $(".fname , .lname").on('input', function(){
+        var fnamevar= $('.fname').val();
+        var lnamevar= $('.lname').val();
+        var fullnamevar = fnamevar + " " + lnamevar;
+        // Print entered value in a div box
+        $(".fullname").val(fullnamevar);
+    });
+});
+</script>
+</body>
---
+++ php_oops/Task6/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="./jquery.js"></script>
+</head> 
+<?php include 'output.php';?>
+
+<body>
+<form  method="post" enctype="multipart/form-data">
+FirstName: <input class ="fname" type="text" name="firstname" required><br>
+LastName: <input class="lname" type="text" name="lastname" required><br>
+FullName: <input class="fullname" type="text" readonly name="fullname"><br>
+Image: <input type="file" name="image"> <br><br>
+TextArea: <Textarea name="content"  cols="30" rows="10"></Textarea>
+Phone: <input type="text" name="phone" value="+91" required> <br><br>
+</form>
+<script>
+    $(document).ready(function(){
+    $(".fname , .lname").on('input', function(){
+        var fnamevar= $('.fname').val();
+        var lnamevar= $('.lname').val();
+        var fullnamevar = fnamevar + " " + lnamevar;
+        // Print entered value in a div box
+        $(".fullname").val(fullnamevar);
+    });
+});
+</script>
+</body>
